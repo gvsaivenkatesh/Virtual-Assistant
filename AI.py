077856208299d294212f0030a5c4acd7df4eddf4
Guddanti Venkateswarlu <125874572+gvsaivenkatesh@users.noreply.github.com>
2025-02-02 13:32:05 +0530

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,12 +1,14 @@
 import pyttsx3
 import speech_recognition as sr
 import webbrowser
+
 def takecommand():
   r = sr.Recognizer()
   with sr.Microphone() as source:
     print("Listening.....")
     r.pause_threshold = 1
     audio = r.listen(source)
+
     try:
       print("Recognizing.....")
       query = r.recognize_google(audio,language='en-in')
@@ -20,26 +22,28 @@ def takecommand():
 def speak(audio):
   engine = pyttsx3.init()
   voices = engine.getProperty('voices')
-  engine.setProperty('voice',voices[0].id)
+  engine.setProperty('voice',voices[1].id)
   engine.say(audio)
   engine.runAndWait()
 def hello():
-  speak("Hello, I am your Virtual assistant. How are you doing.")
+  speak("Hello, I am your Virtual assistant. How can i hep you?")
 def take_user_input():
   hello()
   while True:
     query = takecommand() 
     if "how are you" in query:
-      speak("I am doing Good. How about you ?")
+      speak("I am doing Good")
     elif "what is your name" in query:
       speak("I am Virtual Assistant")
     elif "open page" in query:
-      speak("Opening your web page")
+      speak("Opening web page")
       webbrowser.open("www.codegnan.com")
-    elif "Tata" in query:
+    elif "open youtube" in query:
+      speak("Opening youtube")
+      webbrowser.open("www.youtube.com/")
+    elif "exit" in query:
       speak("Bye Bye, Take Care !")
       exit()
     else:
       speak("I can not get you, speek again !")
 take_user_input()
-    
